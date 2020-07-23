45a4d4cd7911aa6bc9f8a0dbb39d8debc20d54c3
James Le <james3le89@gmail.com>
2020-07-22 17:36:21 -0700

    second commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const morgan = require('morgan')
+const express = require('express')
+const data = require('./data.json')
 
+const PORT = process.env.PORT || 3000;
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.status(200).send('ok');
+});
+
+app.get('/data', (req, res) => {
+  res.json(data);
+});
 
 // finally export the express application
 module.exports = app;
